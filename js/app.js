6cfa92142e2452db3e697a2fe589835a0113cd81
IceTimux <icetimux@gmail.com>
2016-12-03 01:29:17 +0100

    added randomizer function

--- js/app.js
+++ js/app.js
@@ -9,16 +9,29 @@ var assets = {
     'orange': 'assets/orange.jpg',
 };
 
+var randomFruitContainer = [
+    'apple', 
+    'grape',
+    'cherry',
+    'orange'
+];
+
 function initializeGrid()
 {
     $('.grid').css('width', actualSize);
     $('.grid').css('height', actualSize); 
 }
 
+function randomFruit()
+{
+    index = Math.floor(Math.random() * ((randomFruitContainer.length - 1) - 0 + 1)) + 0;
+    return randomFruitContainer[index];
+}
+
 initializeGrid();
 
 for (var y = 1; y <= gridSize; y++) {
     for (var x = 1 ; x <= gridSize; x++) {
-        $('.grid').append('<div class="cell"></div>');
+        $('.grid').append('<div class="cell '+randomFruit()+'"></div>');
     }
 }
