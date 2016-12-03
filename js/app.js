f627d1ec0a6ed582bfee31f42bfa5b9f7097de03
IceTimux <icetimux@gmail.com>
2016-12-03 01:16:13 +0100

    refactored grid initialization into functions

--- js/app.js
+++ js/app.js
@@ -2,5 +2,23 @@ var gridSize = 10;
 var cellSize = 27;
 var actualSize = 10 * 27;
 
-$('.grid').css('width', actualSize);
-$('.grid').css('height', actualSize);
+var assets = {
+    'apple': 'assets/apple.jpg',
+    'grape': 'assets/grape.jpg',
+    'cherry': 'assets/cherry.jpg',
+    'orange': 'assets/orange.jpg',
+};
+
+function initializeGrid()
+{
+    $('.grid').css('width', actualSize);
+    $('.grid').css('height', actualSize); 
+}
+
+initializeGrid();
+
+for (var y = 1; y <= gridSize; y++) {
+    for (var x = 1 ; x <= gridSize; x++) {
+        $('.grid').append('<div class="cell"></div>');
+    }
+}
